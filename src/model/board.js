e15eba748b1501b3f42954cd65f9eb4b7644f5a1
an-in-seek <dktmrorl@naver.com>
2021-07-31 20:32:32 +0900

    오타 수정

--- src/model/board.js
+++ src/model/board.js
@@ -1,12 +1,9 @@
 export default class Board {
-    constructor(boardNo, username, boardTitle, boardSubTitle, boardContent, boardType, comments) {
-      this.boardNo = boardNo;
-      this.username = username;
-      this.boardTitle = boardTitle;
-      this.boardSubTitle = boardSubTitle;
-      this.boardContent = boardContent;
-      this.boardType = boardType;
-      this.comments = comments;
+    constructor(id, title, subTitle, content) {
+      this.id = id;
+      this.title = title;
+      this.subTitle = subTitle;
+      this.content = content;
     }
   }
   
